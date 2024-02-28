0c704b5a3b4189a1daaae8a0f22dd963090d7c5e
OrlandoRomero7 <al18760462@ite.edu.mx>
2024-02-28 14:00:39 -0800

    trabando en encriptacion

---
+++ Data/Models/MemoryRam.cs
@@ -0,0 +1,18 @@
+﻿namespace TescatGlobalServer.Data.Models
+{
+    public class MemoryRam
+    {
+        public Guid ID_RAM { get; set; }
+
+        public string? MODEL { get; set; }
+
+        public string? TYPE_RAM { get; set; }
+
+        public int? SIZE { get; set; }
+
+        public string? SPEED { get; set; }
+
+        public Guid? ID_PC { get; set; }
+    }
+}
+
